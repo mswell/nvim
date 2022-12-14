2bbf6dda7a6589b8dee3650024377fb9f4249c68
Wellington Moraes <wellpunk@gmail.com>
2022-12-14 11:08:40 -0300

    change theme to catppuccin

--- lua/wellpunk/core/colorscheme.lua
+++ lua/wellpunk/core/colorscheme.lua
@@ -1,5 +1,14 @@
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+require("catppuccin").setup({
+	flavour = "mocha", -- latte, frappe, macchiato, mocha
+	background = { -- :h background
+		light = "latte",
+		dark = "mocha",
+	},
+	transparent_background = true,
+})
+
+local status, _ = pcall(vim.cmd, "colorscheme catppuccin-mocha")
 if not status then
-  print("Colorcheme not found!")
-  return
+	print("Colorcheme not found!")
+	return
 end
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -149,6 +149,11 @@ _G.packer_plugins = {
     path = "/home/mswell/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
     url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
   },
+  nvim = {
+    loaded = true,
+    path = "/home/mswell/.local/share/nvim/site/pack/packer/start/nvim",
+    url = "https://github.com/catppuccin/nvim"
+  },
   ["nvim-autopairs"] = {
     loaded = true,
     path = "/home/mswell/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
