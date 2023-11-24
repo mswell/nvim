526b816a4f2d1d19f9a77c3229b07472ce2018ed
Wellington Moraes <wellpunk@gmail.com>
2023-11-24 09:51:06 -0300

    change theme to catppuccin

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -10,13 +10,14 @@ if true then return {} end
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  -- { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
 
