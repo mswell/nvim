5b66f86cb74b56cc171a70b1d1379b8ce83346c2
Wellington Moraes <wellpunk@gmail.com>
2024-02-08 18:47:01 -0300

    change theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,18 @@
-local M = {
-  "LunarVim/darkplus.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
 }
-
-function M.config()
-  vim.cmd.colorscheme("darkplus")
-end
-
-return M
