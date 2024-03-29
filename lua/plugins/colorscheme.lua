b8a54132f2735e93aaa99fdaa3b0894c5821b87c
Wellington Moraes <wellpunk@gmail.com>
2024-03-28 23:30:17 -0300

    change theme to sonokai

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
 return {
   {
-    -- "sainnhe/sonokai",
-    -- priority = 1000,
-    -- config = function()
-    --   vim.g.sonokai_transparent_background = "1"
-    --   vim.g.sonokai_enable_italic = "1"
-    --   vim.g.sonokai_style = "atlantis"
-    -- end,
+    "sainnhe/sonokai",
+    priority = 1000,
+    config = function()
+      vim.g.sonokai_transparent_background = "1"
+      vim.g.sonokai_enable_italic = "1"
+      vim.g.sonokai_style = "shusia"
+    end,
+  },
+  {
+
     "folke/tokyonight.nvim",
     opts = {
       transparent = true,
@@ -17,9 +20,20 @@ return {
     },
   },
   {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.moonflyTransparent = true
+      vim.g.moonflyItalics = true
+    end,
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "sonokai",
     },
   },
 }
