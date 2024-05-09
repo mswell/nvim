9b34377838230bd29f62e5025819aadf7f03634f
Wellington Moraes <wellpunk@gmail.com>
2024-05-09 15:09:07 -0300

    fix null-ls diagnostics error

--- lua/user/none-ls.lua
+++ lua/user/none-ls.lua
@@ -9,7 +9,6 @@ function M.config()
   local null_ls = require "null-ls"
 
   local formatting = null_ls.builtins.formatting
-  local diagnostics =  null_ls.builtins.diagnostics
 
   null_ls.setup {
     debug = false,
@@ -22,7 +21,6 @@ function M.config()
       --   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
       -- },
       -- formatting.eslint,
-      null_ls.builtins.diagnostics.flake8,
       -- diagnostics.flake8,
       null_ls.builtins.completion.spell,
     },
