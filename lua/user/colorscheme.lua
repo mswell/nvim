7ab2500ddcbeea902ba9c8fccae96fbad88bf74b
Wellington Moraes <wellpunk@gmail.com>
2024-08-23 18:46:28 -0300

    change theme to catppuccin

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,14 +1,14 @@
 local M = {
-  "scottmckendry/cyberdream.nvim",
-  lazy = false,
-  priority = 1000,
+  "catppuccin/nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-    require("cyberdream").setup({
-      transparent = true,
+    require("catppuccin").setup({
+      transparent_background = true,
     })
-  vim.cmd.colorscheme "cyberdream"
+  vim.cmd.colorscheme "catppuccin-mocha"
 end
 
 return M
