02508f26ee5a28c09fb341fb42f27128bad933ec
Wellington Moraes <wellpunk@gmail.com>
2024-09-13 19:20:17 -0300

    add ruff

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,15 +1,14 @@
 local M = {
-  "navarasu/onedark.nvim",
-  lazy = false,
-  priority = 1000,
+  "catppuccin/nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-    require("onedark").setup({
-      transparent = true,
-      style = "darker",
+    require("catppuccin").setup({
+      transparent_background = true,
     })
-  vim.cmd.colorscheme "onedark"
+  vim.cmd.colorscheme "catppuccin-mocha"
 end
 
 return M
