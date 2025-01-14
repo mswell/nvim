197f76be77bc223a45fc4a64ba0efc1e2a615771
Wellington Moraes <wellpunk@gmail.com>
2025-01-14 16:23:05 -0300

    remove IA

--- lua/user/plugins/extras.lua
+++ lua/user/plugins/extras.lua
@@ -1,11 +1,5 @@
 local extras = {
   {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({})
-    end,
-  },
-  {
     "LunarVim/bigfile.nvim",
     event = "BufReadPre",
   },
