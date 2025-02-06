8baf020f5bd0ca9fe054b212c0facd7d3760e55f
Wellington Moraes <wellpunk@gmail.com>
2025-02-05 23:51:49 -0300

    change colorscheme

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,14 +1,18 @@
-local M = {
-  'catppuccin/nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+return {
+  {
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('cyberdream').setup {
+        transparent = true,
+        italic_comments = true,
+        borderless_telescope = false,
+        theme = {
+          saturation = 0.6,
+        },
+      }
+      vim.cmd [[colorscheme cyberdream]]
+    end,
+  },
 }
-
-function M.config()
-  require('catppuccin').setup {
-    transparent_background = true,
-  }
-  vim.cmd.colorscheme 'catppuccin-mocha'
-end
-
-return M
