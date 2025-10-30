e6e7edc7b379a8d614c176a2fd6baf6e20aa7d4d
Wellington Moraes <wellpunk@gmail.com>
2025-10-29 23:17:29 -0300

    fix: replace deprecated sign_define with vim.diagnostic.config API

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -44,11 +44,17 @@ return {
     },
   },
   config = function()
-    -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
-    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
-    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
-    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
+    -- Configure diagnostic signs using modern API (Neovim 0.11+)
+    vim.diagnostic.config {
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = ' ',
+          [vim.diagnostic.severity.WARN] = ' ',
+          [vim.diagnostic.severity.INFO] = ' ',
+          [vim.diagnostic.severity.HINT] = '󰌵',
+        },
+      },
+    }
 
     require('neo-tree').setup {
       close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
