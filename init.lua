51b5b4be3e3c97eefc9b09b7279539d780f03c6d
Wellington Moraes <wellpunk@gmail.com>
2025-10-29 22:20:36 -0300

    fix: replace deprecated vim.loop with vim.uv

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ require 'core.snippets' -- Custom code snippets
 
 -- Install package manager
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system {
     'git',
     'clone',
